728d053cb62d728f9b7286a2d6f86ebd9535eb31
Hamas Sabry <s-hamas.ahmed@zewailcity.edu.eg>
2026-05-10 21:08:14 +0300

    Add files via upload

---
+++ armpip/Rst_Clr_FF.sv
@@ -0,0 +1,16 @@
+// flip-flop with async reset and sync clear
+
+module Rst_CL_FF #(parameter WIDTH = 8) (
+input  logic  clk,
+input  logic  reset,
+input  logic  clear,
+input  logic [WIDTH-1:0] d,
+output logic [WIDTH-1:0] q
+);
+
+always_ff @(posedge clk or posedge reset)
+if (reset)      q <= '0 ;
+else if (clear) q <= '0 ;
+else q <= d;
+
+endmodule
