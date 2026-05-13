7b8657e574ad59717758501cc3430cdeda8f4f1d
Yara Hossam <yarahossam201@gmail.com>
2026-05-14 01:19:48 +0300

    Add files via upload

---
+++ arm32pip_modules/Conditional_logic.sv
@@ -0,0 +1,52 @@
+`timescale 1ns/1ps
+module Condition_logic (
+    input logic clk, rst,
+    input logic PCS, 
+    input logic RegW,
+    input logic MemW,
+    input logic BranchE,
+    input logic [1:0] FlagW,
+    input logic [3:0] Cond,
+    input logic [3:0] ALUFlags,
+    output logic PCSrc,
+    output logic RegWrite,
+    output logic BranchTakenE,
+    output logic MemWrite
+);
+    //internal signals
+    logic [1:0] FlagWrite;
+    logic [3:0] Flags;
+    logic CondEx;
+
+    //Registers
+    enable_ff #(2) first_reg (.clk(clk),
+                      .reset(rst),
+                      .en(FlagWrite[0]),
+                      .d(ALUFlags[1:0]),
+                      .q(Flags[1:0])
+                       );
+    
+    enable_ff #(2) sec_reg (.clk(clk),
+                      .reset(rst),
+                      .en(FlagWrite[1]),
+                      .d(ALUFlags[3:2]),
+                      .q(Flags[3:2])
+                       );
+
+
+    //Condition Check
+    Condition_Check condition (.Cond(Cond),
+                            .Flags(Flags),
+                            .CondEx(CondEx)
+                            );
+
+    //Anding
+    assign PCSrc    = PCS & CondEx;
+    assign RegWrite = RegW & CondEx;
+    assign MemWrite = MemW & CondEx;
+    assign BranchTakenE = BranchE & CondEx;
+
+    // Bitwise ANDing
+    assign FlagWrite = {2{CondEx}} & FlagW;
+
+endmodule
